6a8088a11620cd1a6e1b91a04ccafba63a327aae
Wilhem <wilhem.sorek@gmail.com>
2021-10-19 07:18:55 +0200

    Ajout de la fonction modifier

--- src/gsb/modele/dao/VisiteDao.java
+++ src/gsb/modele/dao/VisiteDao.java
@@ -2,7 +2,9 @@ package gsb.modele.dao;
 
 import java.sql.ResultSet;
 
+import gsb.modele.Medecin;
 import gsb.modele.Visite;
+import gsb.modele.Visiteur;
 
 
 public class VisiteDao {
@@ -13,9 +15,9 @@ public class VisiteDao {
 		try {
 			if(reqSeleq.next())
 			{
-				String unMedecin = reqSeleq.getString(4);
-				String unVisiteur = reqSeleq.getString(5);
-				uneVisite = new Visite(reqSeleq.getString(1), reqSeleq.getString(2), reqSeleq.getString(3),MedecinDao.rechercher(unMedecin), VisiteurDao.rechercher(unVisiteur));
+				Medecin unMedecin = MedecinDao.rechercher(reqSeleq.getString(4));
+				Visiteur unVisiteur = VisiteurDao.rechercher(reqSeleq.getString(5));
+				uneVisite = new Visite(reqSeleq.getString(1), reqSeleq.getString(2), reqSeleq.getString(3), unMedecin, unVisiteur);
 						
 			}
 		}
@@ -41,6 +43,24 @@ public class VisiteDao {
 			e.printStackTrace();
 		}
 		return verificationAjout;
+	}
+	
+	public static int modifier(Visite uneVisite)
+	{
+		int verifModif = 0;
+		try 
+		{
+			String reqModif = "update VISITE set DATEVISITE='"+uneVisite.getDate()+"',COMMENTAIRE='"+
+		uneVisite.getCommentaire()+"',MATRICULE='"+uneVisite.getUnVisiteur().getMatricule()+"',CODEMED='"+uneVisite.getUnMedecin().getCodeMed()+
+		"' where REFERENCE ='"+uneVisite.getReference()+"'";
+			verifModif = ConnexionMySql.execReqMaj(reqModif);
+		}
+		catch(Exception e)
+		{
+			System.out.println("erreur sur la requete update");
+			e.printStackTrace();
+		}
+		return verifModif;
 	}
 }
 
