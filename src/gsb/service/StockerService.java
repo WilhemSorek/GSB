fc8fb629969d1c24624433e657155acba94e5b4a
Wilhem <wilhem.sorek@gmail.com>
2021-11-18 17:09:54 +0100

    modif Offrir

--- src/gsb/service/StockerService.java
+++ src/gsb/service/StockerService.java
@@ -3,8 +3,10 @@ package gsb.service;
 import gsb.modele.Stocker;
 import gsb.modele.dao.StockerDao;
 import gsb.modele.Visiteur;
+import java.util.ArrayList;
 import gsb.modele.Medicament;
 
-public class StockerService {
-
+public class StockerService 
+{
+	public static ArrayList<Medicament> retournerListeStock (String code)
 }
