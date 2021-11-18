386dea32af0a613518f698a1cc932496d129e3e9
Wilhem <wilhem.sorek@gmail.com>
2021-11-18 17:25:52 +0100

    classe StockerService

--- src/gsb/service/StockerService.java
+++ src/gsb/service/StockerService.java
@@ -8,5 +8,34 @@ import gsb.modele.Medicament;
 
 public class StockerService 
 {
-	public static ArrayList<Medicament> retournerListeStock (String code)
+	public static ArrayList<Medicament> retournerListeStock (String codeVisiteur)
+	{
+		ArrayList<Medicament> colListeStock = new ArrayList<Medicament>();
+		try {
+			if (codeVisiteur == null) {
+				throw new Exception("Code visiteur non renseigné");
+			}
+			colListeStock = StockerDao.retournerListeStock(codeVisiteur);
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return colListeStock;
+	}
+	
+	public static ArrayList<Visiteur> retournerListeVisiteur (String codeMed)
+	{
+		ArrayList<Visiteur> colVisiteur = new ArrayList<Visiteur>();
+		try {
+			if (codeMed == null)
+			{
+				throw new Exception("Code medicament non renseigné");
+			}
+			colVisiteur = StockerDao.retournerListeVisiteur(codeMed);
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return colVisiteur;
+	}
 }
