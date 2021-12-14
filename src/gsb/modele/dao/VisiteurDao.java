57dd21da12430893a9d36d2bfbc29bd9389b96c8
Wilhem <wilhem.sorek@gmail.com>
2021-12-14 16:09:36 +0100

    Modification de la fonction recherche

--- src/gsb/modele/dao/VisiteurDao.java
+++ src/gsb/modele/dao/VisiteurDao.java
@@ -15,7 +15,7 @@ public class VisiteurDao {
 		try {
 			if (reqSelection.next()) {
 				String unCp = reqSelection.getString(7);
-				unVisiteur = new Visiteur(reqSelection.getString(1), reqSelection.getString(2), reqSelection.getString(3), reqSelection.getString(4), reqSelection.getString(5), reqSelection.getString(6), LocaliteDao.rechercher(unCp), reqSelection.getString(8),reqSelection.getInt(12), reqSelection.getString(9), reqSelection.getString(10));
+				unVisiteur = new Visiteur(reqSelection.getString(1), reqSelection.getString(2), reqSelection.getString(3), reqSelection.getString(4), reqSelection.getString(5), reqSelection.getString(6), LocaliteDao.rechercher(unCp), reqSelection.getString(8),reqSelection.getInt(11), reqSelection.getString(9), reqSelection.getString(10));
 				};
 			}
 		catch(Exception e) {
